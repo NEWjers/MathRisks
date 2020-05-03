676fca9deead44e1fdf3362d297698fbc6520bf6
Serj Pipuk <serjsonet@gmail.com>
2020-05-03 19:07:03 +0300

    6

---
+++ Ex5/Ex5/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Ex5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbTask = new System.Windows.Forms.PictureBox();
+            this.lblRezult = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTask)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbTask
+            // 
+            this.pbTask.Location = new System.Drawing.Point(12, 12);
+            this.pbTask.Name = "pbTask";
+            this.pbTask.Size = new System.Drawing.Size(665, 376);
+            this.pbTask.TabIndex = 0;
+            this.pbTask.TabStop = false;
+            // 
+            // lblRezult
+            // 
+            this.lblRezult.AutoSize = true;
+            this.lblRezult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRezult.Location = new System.Drawing.Point(237, 414);
+            this.lblRezult.Name = "lblRezult";
+            this.lblRezult.Size = new System.Drawing.Size(0, 13);
+            this.lblRezult.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(730, 212);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Calculate";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblRezult);
+            this.Controls.Add(this.pbTask);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbTask)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbTask;
+        private System.Windows.Forms.Label lblRezult;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Ex5/Ex5/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pbTask.Image = Image.FromFile("C:\\Users\\Asus\\Desktop\\Универ\\Мельник\\MathRisks\\Ex5\\Ex5\\2020-05-02 21_11_43-Window.png");
+        }
+
+        public double ColumnMax(double[,] vs, int columnNumber)
+        {
+            double max = vs[0, columnNumber];
+            for(int i = 0; i < 5; i++)
+            {
+                if( vs[i, columnNumber] >= max)
+                {
+                    max = vs[i, columnNumber];
+                }
+            }
+            return max;
+        }
+
+        public double RowMax(double[,] vs, int rowNumber)
+        {
+            double max = vs[rowNumber, 0];
+            for(int i = 0; i < 3; i++)
+            {
+                if(vs[rowNumber, i] >= max)
+                {
+                    max = vs[rowNumber, i];
+                }
+            }
+            return max;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            double[,] inputMatrix = new double[,]
+            {
+                {2.5, 3.5, 4.0 },
+                {1.5, 2.0, 3.5 },
+                {3.5, 8.0, 2.5 },
+                {7.5, 1.5, 3.5 },
+                {8.5, 1.5, 4.0 }
+            };
+
+            double l1 = ColumnMax(inputMatrix, 0);
+            double l2 = ColumnMax(inputMatrix, 1);
+            double l3 = ColumnMax(inputMatrix, 2);
+
+            double[,] bufferMatrix = new double[5, 3];
+
+            for(int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (j == 0)
+                    {
+                        bufferMatrix[i, j] = l1 - inputMatrix[i, j];
+                    }
+                    if (j == 1)
+                    {
+                        bufferMatrix[i, j] = l2 - inputMatrix[i, j];
+                    }
+                    if (j == 2)
+                    {
+                        bufferMatrix[i, j] = l3 - inputMatrix[i, j];
+                    }
+                }
+            }
+
+            double[] r = new double[]
+            {
+                RowMax(bufferMatrix, 0),
+                RowMax(bufferMatrix, 1),
+                RowMax(bufferMatrix, 2),
+                RowMax(bufferMatrix, 3),
+                RowMax(bufferMatrix, 4)
+            };
+
+            double min = r[0];
+            int result = 0;
+
+            for(int i = 0; i < r.Length; i++)
+            {
+                if (r[i] <= min)
+                {
+                    min = r[i];
+                    result = i;
+                }
+            }
+
+            lblRezult.Text = "r" + (result + 1) + "=" + " " + min.ToString() + " " + "Отже оберемо варіант: " + (result + 1);
+        }
+    }
+}
